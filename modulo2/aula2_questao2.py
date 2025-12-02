813f5548f3056df04dcf3c484aa5e90eb75f2980
henrique-b7 <bruno21.sd2@gmail.com>
2025-12-02 19:57:35 -0300

    aula2

---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,12 @@
+vres = "O resultado é:"
+v1 = 10
+v2 = 3.5
+vsoma = v1 + v2
+print(vres)
+print(type(vres))
+print(v1)
+print(type(v1))
+print(v2)
+print(type(v2))
+print(vsoma)
+print(type(vsoma))
